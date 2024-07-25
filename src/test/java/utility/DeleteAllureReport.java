ec7e71acb272422677febe6da488f91c478bd9b8
Vaibhav <kitukale.vaibhav26@outlook.com>
2024-07-25 12:14:39 +0530

    comment add for webhhok testing

--- src/test/java/utility/DeleteAllureReport.java
+++ src/test/java/utility/DeleteAllureReport.java
@@ -9,6 +9,7 @@ public class DeleteAllureReport {
     @Step("delete previous allure report")
     public static void deleteAllureReports(){
         // comment for testinmg configuratiom set
+        //setup succesfully
                 File directory = new File("allure-results");
                 if (directory.exists()) {
                     File[] files = directory.listFiles();
